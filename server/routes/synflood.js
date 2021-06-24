90c0de1176dc34c14f2eaa4a3f1e15c500f4527f
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-24 16:34:09 +0200

    created expressjs server

---
+++ server/routes/synflood.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var sqlite3 = require("sqlite3");
+var path = require("path");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  let db = new sqlite3.Database(path.join(__dirname, "ids.db"), (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the chinook database.");
+    // res.send("API is working properly");
+  });
+  db.all(`SELECT * FROM syn_flood`, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(rows);
+    res.send(rows);
+  });
+});
+
+module.exports = router;
